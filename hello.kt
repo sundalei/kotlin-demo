50fd500c90e26a3ab07a4a957a51adb5a3a366c5
DaLei Sun <sundalei1988@gmail.com>
2019-12-26 14:38:36 +0800

    function usage of kotlin

---
+++ hello.kt
@@ -0,0 +1,26 @@
+package my.demo
+
+fun main(args: Array<String>) {
+	//println("hello, world!");
+	//val result = sum(1, 2);
+	//println(result);
+	//printSum(1, 2);
+}
+
+/*
+fun sum(a: Int, b: Int): Int {
+	return a + b
+}
+*/
+
+fun sum(a: Int, b: Int) = a + b
+
+/*
+fun printSum(a: Int, b: Int): Unit {
+	println("sum of $a and $b is ${a + b}")
+}
+*/
+
+fun printSum(a: Int, b: Int) {
+	println("sum of $a and $b is ${a + b}")
+}
